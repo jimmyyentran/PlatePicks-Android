7b7917a2cd956af9640eee09e872272d25b12adc
Jimmy Tran <jtran064@ucr.edu>
2016-04-27 19:52:24 -0700

    unit test

---
+++ app/src/test/java/AWSIntegratorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.foodtinder.util.AWSIntegrator;
+
+public class AWSIntegratorTest {
+
+    @Test
+    public void tester(){
+        assertTrue(true);
+//        AWSIntegrator.call("hello-world", "{\n  \"key1\" : \"value1\",\n  \"key2\" : \"value2\",\n  \"key3\" : \"value3\"\n}");
+//        assertEquals("value1", AWSIntegrator.call("hello-world", "{\n  \"key1\" : \"value1\",\n  \"key2\" : \"value2\",\n  \"key3\" : \"value3\"\n}"));
+    }
+}
