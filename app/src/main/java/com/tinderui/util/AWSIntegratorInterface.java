114a0d9d942e4d534cd69c749ce7d7dfea220784
Jimmy Tran <jtran064@ucr.edu>
2016-04-28 14:29:25 -0700

    add interface

---
+++ app/src/main/java/com/tinderui/util/AWSIntegratorInterface.java
@@ -0,0 +1,10 @@
+package com.tinderui.util;
+
+import java.util.Objects;
+
+/**
+ * Created by jimmytran on 4/28/16.
+ */
+public interface AWSIntegratorInterface {
+    public void doSomethingWithResults(Objects ob);
+}
