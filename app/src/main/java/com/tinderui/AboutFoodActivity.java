3af8bcc5788e60c7f208bc679a54fef1dc4d472a
jkinc002 <jkinc002@ucr.edu>
2016-05-11 22:48:26 -0700

    updated "about-food" activity. more fonts.

--- app/src/main/java/com/tinderui/AboutFoodActivity.java
+++ app/src/main/java/com/tinderui/AboutFoodActivity.java
@@ -1,11 +1,17 @@
 package com.tinderui;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.foodtinder.R;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by pokeforce on 4/24/16.
@@ -18,10 +24,43 @@ public class AboutFoodActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutfood);
 
-        /* Give the toolbar a "back" (aka "up") button. Goes back "up" to previous activity because
-         * of specification in onOptionsItemSelected() */
-        //noinspection ConstantConditions
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        /* set custom fonts */
+        Typeface quicksand = Typeface.createFromAsset(getAssets(), "fonts/Quicksand-Regular.otf");
+        Typeface archistico_bold = Typeface.createFromAsset(getAssets(), "fonts/Archistico_Bold.ttf");
+        Typeface ham_heaven = Typeface.createFromAsset(getAssets(), "fonts/Hamburger_Heaven.TTF");
+
+        TextView bar_name = (TextView) findViewById(R.id.bar_title);
+        bar_name.setTypeface(ham_heaven);
+
+        TextView restaurant = (TextView) findViewById(R.id.restaurant_name);
+        restaurant.setTypeface(archistico_bold);
+
+        TextView food = (TextView) findViewById(R.id.food_name);
+        TextView tmp1 = (TextView) findViewById(R.id.street);
+        tmp1.setTypeface(quicksand);
+        tmp1 = (TextView) findViewById(R.id.city_state);
+        tmp1.setTypeface(quicksand);
+        tmp1 = (TextView) findViewById(R.id.zip_code);
+        tmp1.setTypeface(quicksand);
+        food.setTypeface(quicksand);
+
+
+        /* handle font size for restaurant name */
+        int str_length = restaurant.getText().length();
+
+        if(str_length <= 15){
+            restaurant.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 27);
+        }
+        else if(str_length <= 25){
+            restaurant.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 21);
+        }
+        else if(str_length <= 35){
+            restaurant.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 15);
+        }
+        else{
+            restaurant.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10);
+        }
+
     }
 
     /* OnOptionsItemSelected():
@@ -36,5 +75,9 @@ public class AboutFoodActivity extends AppCompatActivity {
             default:
                 return false;
         }
+    }
+
+    public void backArrow (View view){
+        super.onBackPressed();
     }
 }
