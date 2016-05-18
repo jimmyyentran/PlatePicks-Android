1d9cbacd6db20dff1bcfb119a1b14029d6ac488e
Jimmy Tran <jtran064@ucr.edu>
2016-05-18 01:03:31 -0700

    working scan for comment

--- app/src/main/java/com/platepicks/dynamoDB/TableComment.java
+++ app/src/main/java/com/platepicks/dynamoDB/TableComment.java
@@ -5,6 +5,9 @@ import android.util.Log;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.mobile.AWSMobileClient;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpression;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.PaginatedScanList;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.platepicks.dynamoDB.nosql.CommentDO;
 import com.platepicks.objects.FoodReceive;
 import com.platepicks.objects.Location;
@@ -12,13 +15,16 @@ import com.platepicks.objects.Location;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by alyza on 5/17/16.
  */
 public class TableComment {
-
+    private final static String LOG_TAG = TableComment.class.getSimpleName();
     public static void insertComment(String userId, String foodId, String content) throws AmazonClientException {
         System.out.println("Inserting comment");
 
@@ -52,6 +58,39 @@ public class TableComment {
 //        CommentDO CommentToGet;
 //        CommentToGet = mapper.load(CommentDO.class, userId);
 //        System.out.println("Got comment: " + CommentToGet.getContent());
+
+    }
+
+    public static void getCommentsFromFoodID(String foodId) {
+        Log.d(LOG_TAG, "Getting the comments from foodId");
+
+        PaginatedScanList<CommentDO> results;
+        Iterator<CommentDO> resultsIterator;
+
+        final DynamoDBMapper mapper = AWSMobileClient.defaultMobileClient().getDynamoDBMapper();
+
+        // These hold attribute actual names
+        final Map<String, String> filterExpressionAttributeNames = new HashMap<>();
+        filterExpressionAttributeNames.put("#key", "foodId");
+
+        // These hold attributes values
+        final Map<String, AttributeValue> filterExpressionAttributeValues = new HashMap<>();
+        filterExpressionAttributeValues.put(":value",
+                new AttributeValue().withS(foodId));
+
+        // Setup Scan
+        final DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+                .withFilterExpression("#key = :value")
+                .withExpressionAttributeNames(filterExpressionAttributeNames)
+                .withExpressionAttributeValues(filterExpressionAttributeValues);
+
+        // Execute Scan
+        results = mapper.scan(CommentDO.class, scanExpression);
+        if (results != null) {
+            for (CommentDO returnComment : results){
+                System.out.println(returnComment.getContent());
+            }
+        }
 
     }
 }
--- app/src/test/java/TableCommentTest.java
+++ app/src/test/java/TableCommentTest.java
@@ -11,6 +11,7 @@ import com.platepicks.dynamoDB.nosql.CommentDO;
 import com.platepicks.dynamoDB.nosql.ListDO;
 import com.platepicks.dynamoDB.nosql.SampleDataGenerator;
 
+import static com.platepicks.dynamoDB.TableComment.getCommentsFromFoodID;
 import static com.platepicks.dynamoDB.TableComment.insertComment;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,7 +25,7 @@ import java.util.Arrays;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest="src/main/AndroidManifest.xml", sdk = 18) //sdk level may change
-public class InsertCommentTest {
+public class TableCommentTest {
 
     private final DynamoDBMapper mapper = AWSMobileClient.defaultMobileClient().getDynamoDBMapper();;
 
@@ -37,8 +38,11 @@ public class InsertCommentTest {
     public void DynamodbInsertToFoodTest() {
         // Start all test input as 'test' so it can be easily removed from table later
         insertComment("testUserId1", "testFoodID1", "testCommentContent1");
+    }
 
-
+    @Test
+    public void getCommentsFromFoodIDTest(){
+        getCommentsFromFoodID("JEsHU0JV2G_47ICJa3uv1A");
     }
 
 
